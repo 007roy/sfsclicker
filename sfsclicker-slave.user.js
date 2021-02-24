0fdf4f1e1e3ccaa72d1fd3fd0cf9c6929d7ecead
007roy <troy.bennett@gmail.com>
2021-02-24 11:12:13 -0800

    update

--- sfsclicker-slave.user.js
+++ sfsclicker-slave.user.js
@@ -20,6 +20,8 @@ if(playerList.length <=0) return;
 var player = playerList[playerListIndex];
 var playerDetails = GM_getValue('PlayerDetails', []);
 
+setTimeout(makePlayerList,1000);
+
 setTimeout(function(){
       if(playerListIndex > 2){
             //done
@@ -33,6 +35,24 @@ setTimeout(function(){
             window.location.href = 'https://www.mysfs.net/home/index/'+playerList[playerListIndex].playerId;
       }
 }, 1000);
+
+function makePlayerList(){
+      var playerList = [];
+      var now = jQuery.now();
+      jQuery.post('players/get_players_listing',{page: 1, player_type: 3, player_seach_string: ''},function(data){
+        data.msg.forEach(function(item,index){
+          var id = item.player_id;
+          var lastOnline = item.player_connection;
+          var url = 'https://www.mysfs.net/home/index/'+id;
+          var daysOld = ((now/1000 - lastOnline)/1440).toFixed(1);
+          console.log(lastOnline);
+          var pls = `{'playerId':${id}, 'nickName':'${item.nick_name}', 'lastOnline': ${daysOld}}`;
+          playerList.push(pls);
+        });
+        
+        GM_setValue('PlayerList', playerList);
+      },"json");
+}
 
 function parsePage(){
       var key = player.playerId;
