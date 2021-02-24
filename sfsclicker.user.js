760ffdf7d457da1f403aa0891417e7f432c80ff5
007roy <troy.bennett@gmail.com>
2021-02-24 10:47:34 -0800

    update ver

--- sfsclicker.user.js
+++ sfsclicker.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        SFS Clicker - mysfs.net
 // @namespace   https://violentmonkey.github.io
-// @version     1.0
+// @version     1.1
 // @description  try to take over the world!
 // @author       You
 // @match       https://www.mysfs.net/players
