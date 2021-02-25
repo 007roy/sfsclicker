c6bc4e9dd5ac153c3ece106e6d304dfb524bfd30
007roy <troy.bennett@gmail.com>
2021-02-25 15:34:37 -0800

    up

--- sfsclicker.user.js
+++ sfsclicker.user.js
@@ -14,72 +14,94 @@
 // ==/UserScript==
 
 var jQuery = window.jQuery;
-setTimeout(initUI,2000);
 
-
+switch(GM_getValue('CLICKER_STATE', 0)){
+  case 0: //idle
+    setTimeout(initUI,2000);
+    break;
+  case 1: //buy everything
+    GotaGetThemAll();
+    break;
+  case 3: //work um
+    break;
+}
 
 function makePlayerList(){
-  //var playerList = [];
-  //var now = jQuery.now();
   jQuery.post('https://www.mysfs.net/players/get_players_listing',
     {page: 1, player_type: 3, player_seach_string: ''},
     function(data){
-    /*
-    data.msg.forEach(function(item,index){
-      var id = item.player_id;
-      var lastOnline = item.player_connection;
-      var url = 'https://www.mysfs.net/home/index/'+id;
-      var daysOld = ((now/1000 - lastOnline)/1440).toFixed(1);
-      var pls = {'playerId':id, 'nickName':item.nick_name, 'lastOnline': daysOld};
-      playerList.push(pls);
-    });*/
-
-    GM_setValue('PlayerList', data.msg);
-    //GM_setValue('MINE_DATA', true);
-    //location.reload();
+      GM_setValue('PlayerList', data.msg);
     },"json");
 }
 
-function GetPageID(){
+function GetPageId(){
   return parseInt(window.location.pathname.match(/[0-9]+/g)[0]);
 }
 
 function initUI(){
+  var pageId = GetPageId();
   jQuery('.logo').after(`<div id='sfsclicker'></div>`);
   jQuery('#sfsclicker').css({'font-size': '14px', 'color': '#fff'});
   jQuery('#sfsclicker').append("<button id='clickerbutton'>Do Stuff</button>")
-  jQuery('#sfsclicker').append(GetPageId());
+  jQuery('#sfsclicker').append(pageId);
   jQuery('#sfsclicker').append(" thing: " + (jQuery("#current_time").val() - jQuery("#sold_timer_"+pageId).val()));
-  jQuery('#clickerbutton').on("click", makePlayerList);
+  jQuery('#clickerbutton').on("click", StartGetThemAll);
 }
-
-function gotaGetThemAll(){
-  if(!GM_getValue('GetUmAll',false))return;
+function StartGetThemAll(){
+  GM_setValue('CLICKER_STATE',1);
+  GotaGetThemAll();
+}
+function GotaGetThemAll(){
   var playerListIndex = GM_getValue('PlayerListIndex',0);
   var playerList = GM_getValue('PlayerList',[]);
-  if(playerList.length <=0) return;  //TODO refresh list
+  if(playerList.length <=0) makePlayerList();
   var player = playerList[playerListIndex];
-  if(player.playerId != GetPageID()) window.location.href('https://www.mysfs.net/home/index/' + player.playerId);
-  //TODO update ui
-  var getUm = setInterval(function(){
-      //check if we buy
-      if(jQuery('.buy_li_'+player.playerId).css('display')=='none') return;
-      //buy
-      //jQuery('#buy_to_any_player').click();
-      console.log('buy '+player.playerId);
-      //TODO set 5min timer
-      clearInterval(getUm);
-  },100);
 
+  //TODO update ui
+  var uiReady = setInterval(()=>{
+    if(jQuery('.buy_li_'+player.player_id)!=undefined) clearInterval(uiReady);
+  }, 100);
+  
+  if(player.player_id != GetPageId()){
+    LoadPlayerPage(player.player_id);
+  }
+  
+  if(jQuery('.auction_timer').text()=='--:--'){
+    console.log('beep');
+    if(jQuery('.buy_li_'+player.player_id).css('display')=='list-item'){
+      console.log('boop');
+          //buy
+          //jQuery('#buy_to_any_player').click();
+          console.log('buy '+player.player_id);
+          RecordAuctionTimer(player.player_id);
+    }
+  }
   //next?
   var nextId = playerListIndex + 1;
   if(nextId >= playerList.length){
     //done
-    GM_setValue('GetUmAll', false);
+    GM_setValue('CLICKER_STATE', 0);
     jQuery('.logo').click();
   }
   //next
   GM_setValue('PlayerListIndex', nextId);
-  window.location.href('https://www.mysfs.net/home/index/' + playerList[nextId].playerId);
+  LoadPlayerPage(playerList[nextId].player_id);
+  
 }
+
+function RecordAuctionTimer(id){
+  console.log('record buy');
+  var auctionTimers = GM_getValue('AUCTION_TIMERS',[]);
+  auctionTimers.push({'id':id, 'time': jQuery.now()+300000});
+  GM_setValue('AUCTION_TIMERS',auctionTimers);
+}
+
+function LoadPlayerPage(id){
+  //window.location.href ='https://www.mysfs.net/home/index/' + id;
+  console.log('https://www.mysfs.net/home/index/' + id);
+  
+}
+
+
+
 
